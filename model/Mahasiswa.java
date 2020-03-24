089cdffe0b1b44490127cc2a3e29020dc507180b
sitihalimatus <hlmtss18@gmail.com>
2020-03-24 21:28:42 +0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,14 @@
+package model;
+
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("-- Data Mahasiswa ---------------");
+  System.out.println("NPM   : 18630174");
+  System.out.println("Nama  : Siti Halimatussa'diah");
+  System.out.println("---------------------------------");
+  System.out.println("Kelas : 4C");
+  System.out.println("---------------------------------");
+ }
+}
